e5b323a074b48927b51e59bf4af0ac919ec57905
MC4002 <maygec@nycstudents.net>
2022-11-04 12:30:03 -0400

    Structure-Base

---
+++ anime/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="Carousel"></div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            Cards: [
+                {
+                    Title:"Title of Anime",
+                    Description:"(Synopsis)",
+                    ImgLink:"",
+                },
+                {
+                    Title:"Title of Anime",
+                    Description:"(Synopsis)",
+                    ImgLink:"",
+                },
+                {
+                    Title:"Title of Anime",
+                    Description:"(Synopsis)",
+                    ImgLink:"",
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+</style>
+
