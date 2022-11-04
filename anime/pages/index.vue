50eb73160db24d7c24ede2eb3734bf8a9ebd8dcb
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-11-04 12:02:03 -0400

    .

--- anime/pages/index.vue
+++ anime/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div id="anime">
+    <div class="ex"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +11,9 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+<style>
+@import url("../assets/global.css");
+.ex{
+  background-color: var(--primary);
+}
+</style>
