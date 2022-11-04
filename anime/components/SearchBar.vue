c0c5510d1e93f3df1dae8224a255a00924aa4820
Anqi-Tech <anqihu1107@gmail.com>
2022-11-04 12:30:39 -0400

    created input

---
+++ anime/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" placeholder="Search anime...">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+input {
+  background: red;
+}
+</style>
