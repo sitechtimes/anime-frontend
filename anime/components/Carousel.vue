d28564f8004e926aeb85b774240c417c12ceac18
MC4002 <maygec@nycstudents.net>
2022-11-07 12:31:28 -0500

    11/07 - Attempts were made

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -1,34 +1,49 @@
 <template>
-<div class="Carousel"></div>
+<div class="Carousel">
+    <div class="Space">
+        <button @click="Previous">Prev</button>
+        <div class="CarouselItem">
+            <h2 class="Title">Title of Anime</h2>
+            <h3 class="Synopsis">Description</h3>
+            <img src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="Img of Anime"/>
+        </div>
+        <div class="CarouselItem">
+            <h2 class="Title">Title of Anime</h2>
+            <h3 class="Synopsis">Description</h3>
+            <img src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="Img of Anime"/>
+        </div>
+        <div class="CarouselItem">
+            <h2 class="Title">Title of Anime</h2>
+            <h3 class="Synopsis">Description</h3>
+            <img src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="Img of Anime"/>
+        </div>
+        <button @click="Next">Next</button>
+    </div>
+</div>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            Cards: [
-                {
-                    Title:"Title of Anime",
-                    Description:"(Synopsis)",
-                    ImgLink:"",
-                },
-                {
-                    Title:"Title of Anime",
-                    Description:"(Synopsis)",
-                    ImgLink:"",
-                },
-                {
-                    Title:"Title of Anime",
-                    Description:"(Synopsis)",
-                    ImgLink:"",
-                },
-            ]
-        }
-    }
 
+export default {
+    name: "Carousel",
+    data () {
+        return {}
+    }
 }
 </script>
 
 <style scoped>
+h2, h3 {
+    color: var(--white);
+}
+.Carousel {
+    overflow: hidden;
+}
+.Space {
+    white-space: nowrap;
+}
+.CarouselItem {
+    display: inline-flex;
+}
 </style>
 
