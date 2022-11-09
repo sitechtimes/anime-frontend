a8904aab3c8f3794bcb44100ff3233a5c68b9111
KingJelloX <90670468+KingJelloX@users.noreply.github.com>
2022-11-09 12:31:32 -0500

    making cards

---
+++ anime/components/AnimeCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img v-bind:src="`${img}`" alt="">
+        <div>
+            <p>{{episode}}</p>
+            <p>{{format}}</p>
+        </div>
+        <h2>{{title}}</h2>
+    </div>
+</template>
--- anime/components/Component.vue
+++ anime/components/Component.vue
@@ -1,18 +1,37 @@
 <template>
-    <div class="container">
-        <div class="header">
-            <h2></h2>
-            <div class="header-pages">
-                <button></button>
-                <button></button>
-            </div>
-        </div>
-        <div class="content"></div>
+  <div class="container">
+    <div class="header">
+      <h2></h2>
+      <div class="header-pages">
+        <button></button>
+        <button></button>
+      </div>
     </div>
+    <div class="content">
+      <AnimeCard
+        v-for="anime in trending"
+        :key="anime.id"
+        :img="anime.img"
+        :ep="anime.episode"
+        :format="anime.format"
+        :title="anime.title"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import AnimeCard from './AnimeCard.vue'
+
+export default {
+  components: {
+    AnimeCard,
+  },
+}
+</script>
 
 <style scoped>
 .container {
-    color: var(--primary)
+  color: var(--primary);
 }
 </style>
