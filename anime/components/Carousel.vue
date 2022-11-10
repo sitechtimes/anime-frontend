5f9cca278c563e86e6adea427fa74dbe5e063cd2
MC4002 <maygec@nycstudents.net>
2022-11-10 12:29:59 -0500

    11/10 - More Attempts were Made

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="Carousel">
     <div class="Space">
-        <button @click="Previous">.</button>
+        <span class="BTN" @click="TBD">.</span>
         <div class="Carousel-Item" v-for="Card in Cards" :key="Card">
-            <div class="Title"> {{Card.Title}} </div>
-            <div class="Text"> {{Card.Description}} </div>
-            <img src="" alt="TBD"/>
+            <h1> {{Card.Title}} </h1>
+            <h2> {{Card.Description}} </h2>
+            <img class="Img" src="" alt="TBD"/>
         </div>
-        <button @click="Next">.</button>
+        <span class="BTN" @click="TBD">.</span>
     </div>
 </div>
 </template>
@@ -41,11 +41,22 @@ export default {
 </script>
 
 <style scoped>
-.Carousel {
-    overflow: hidden;
+.Img {
+    height: 45%;
+}
+.BTN {
+    margin: 5px;
 }
 .Space {
     white-space: nowrap;
+}
+.Carousel {
+    margin: 0%;
+    padding: 0%;
+    overflow: hidden;
+    text-align: center;
+    color: var(--white);
+    background-color: var(--bg-primary);
 }
 .CarouselItem {
     display: inline-flex;
