e8e66f9abc4a8477692785a1096d589e332a3b36
Anqi-Tech <71527898+Anqi-Tech@users.noreply.github.com>
2022-11-20 23:46:54 -0500

    only shows top 5 search results

--- anime/components/SearchBar.vue
+++ anime/components/SearchBar.vue
@@ -25,10 +25,15 @@ export default {
       text: "",
       animes: [
         {title: "One Punch Man", ageRating: "PG-13", stars: 8.50, releaseDate: "Oct 5, 2015"},
-        {title: "Spy x Family", ageRating: "G", stars: 9.99, releaseDate: "April 9, 2022"},
-        {title: "Chainsaw Man", ageRating: "PG-13", stars: 8.24, releaseDate: "Oct 20, 1999"},
-        {title: "Summertime Render", ageRating: "PG-13", stars: 9.61, releaseDate: "Oct 20, 1999"},
-        {title: "Is It Wrong To Try To Pick Up Girls In A Dungeon?", ageRating: "E", stars: 7.69, releaseDate: "Oct 20, 1999"},
+        {title: "Spy x Family", ageRating: "G", stars: 9.99, releaseDate: "Apr 9, 2022"},
+        {title: "Chainsaw Man", ageRating: "PG-13", stars: 8.24, releaseDate: "Oct 11, 2022"},
+        {title: "Summertime Render", ageRating: "PG-13", stars: 9.61, releaseDate: "Apr 15, 2022"},
+        {title: "Is It Wrong To Try To Pick Up Girls In A Dungeon?", ageRating: "E", stars: 7.69, releaseDate: "Apr 4, 2015"},
+        {title: "I Spent Hours of my Life Doing Spiral Abyss and all I got was an Extra 50 Primogems", ageRating: "E", stars: 9.89, releaseDate: "June 9, 2022"},
+        {title: "Some Mid Isekai Anime", ageRating: "G", stars: 2.56, releaseDate: "Aug 19, 2021"},
+        {title: "I went on a Field Trip Where I Touched Grass For the First Time in Years", ageRating: "PG-13", stars: 7.86, releaseDate: "Jan 17, 2012"},
+        {title: "The Pi in the Sky", ageRating: "G", stars: 9.90, releaseDate: "Mar 14, 1592"},
+        {title: "Genshin Impact The Anime", ageRating: "R", stars: 9.27, releaseDate: "TBA"},
         ],
     }
   },
@@ -37,7 +42,7 @@ export default {
       if (this.text) {
         return this.animes.filter((anime) => {
           return this.text.toLowerCase().split().every(search => anime.title.toLowerCase().includes(search))
-        })
+        }).slice(0,5)
       }
     }
   }
