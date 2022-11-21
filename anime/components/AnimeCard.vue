84b64a7d37bf07845d366df9c80387eb1520def6
KingJelloX <90670468+KingJelloX@users.noreply.github.com>
2022-11-21 12:31:15 -0500

    added ep box to anime card

--- anime/components/AnimeCard.vue
+++ anime/components/AnimeCard.vue
@@ -3,10 +3,23 @@
     <img class="anime-img" :src="`${img}`" alt="" />
 
     <div class="container-bar">
-      <p id="ep">{{ episode }}</p>
+      <p id="ep">
+        <svg
+          width="50"
+          height="16"
+          viewBox="0 0 46 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 1C0 0.447715 0.447715 0 1 0H44.6975C45.3548 0 45.8333 0.623321 45.6635 1.25833L42.4886 13.1313C42.3717 13.5688 41.9754 13.873 41.5226 13.873H1C0.447717 13.873 0 13.4253 0 12.873V1Z"
+            fill="#FF645A"
+          />
+        </svg>
+      </p>
       <p id="format">{{ format }}</p>
     </div>
-    <p class="title">{{ title }}</p>
+    <h5 class="title">{{ title }}</h5>
   </div>
 </template>
 
@@ -53,26 +66,27 @@ img {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: black;
+  align-items: center;
+  background-color: var(--bg-primary);
   padding: 0.5rem;
   border-radius: 0 0 0.5rem 0.5rem;
 }
 
 .title {
   width: 100%;
-  color: white;
+  color: var(--white);
+  font-size: var(--h5);
 }
 
 #ep {
-  background-color: var(--primary);
-  font-size: 0.8rem;
+  width: 1.5rem;
+  font-size: var(--h6);
   color: var(--white);
-  padding: 0.2rem 0.5rem;
 }
 
 #format {
   color: #f5f5f5;
-  font-size: 0.8rem;
+  font-size: var(--h6);
   padding: 0.2rem 0.5rem;
 }
 </style>
--- anime/components/TrendingBox.vue
+++ anime/components/TrendingBox.vue
@@ -3,8 +3,38 @@
     <div class="header">
       <h2 class="header-title">Trending</h2>
       <div class="header-pages">
-        <button class="page-button"><</button>
-        <button class="page-button">></button>
+        <button class="page-button">
+          <svg
+            width="20"
+            height="34"
+            viewBox="0 0 20 34"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M20.0001 2.51566L17.865 0.397673C17.4729 0.00871778 16.8372 0.00871849 16.4452 0.397674L0.401007 16.3137C0.00891876 16.7027 0.00892067 17.3333 0.401007 17.7222L16.4452 33.6383C16.8372 34.0272 17.4729 34.0272 17.865 33.6383L20.0001 31.5203L5.38101 17.018L20.0001 2.51566Z"
+              fill="#D9D9D9"
+            />
+          </svg>
+        </button>
+        <button class="page-button">
+          <svg
+            width="20"
+            height="34"
+            viewBox="0 0 20 34"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M6.11144e-05 31.2102L2.1327 33.3093C2.52631 33.6967 3.16448 33.6967 3.55809 33.3093L19.5987 17.5212C19.9923 17.1338 19.9924 16.5056 19.5987 16.1182L3.55809 0.330086C3.16448 -0.0573273 2.52632 -0.0573273 2.1327 0.330086L6.10352e-05 2.42916L14.6208 16.8197L6.11144e-05 31.2102Z"
+              fill="#D9D9D9"
+            />
+          </svg>
+        </button>
       </div>
     </div>
     <div class="content">
@@ -122,8 +152,6 @@ export default {
 </script>
 
 <style scoped>
-
-
 .header {
   display: flex;
   justify-content: space-between;
