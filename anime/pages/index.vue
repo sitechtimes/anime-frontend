6112cce94452168aba42dc7da887388d40c30e3a
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-12-02 13:54:47 -0500

    fixed some stuff

--- anime/pages/index.vue
+++ anime/pages/index.vue
@@ -10,7 +10,7 @@
   <script lang="ts">
 //   import Vue from 'vue'
   //import ComponentVue from '../components/Component.vue'
-  import NavBar from './components/navbar/NavBar.vue'
+  import NavBar from '../components/navbar/NavBar.vue'
 
   export default ({
     name: 'IndexPage',
@@ -23,11 +23,12 @@
   })
   </script>
   <style>
-  @import url("./assets/global.css");
+  @import url("../assets/global.css");
+   /* @import url("./assets/global.css"); */
   .ex{
     background-color: var(--primary);
   }
   h1{
     font-size:var(--h1)
-  }
+  } 
   </style>
