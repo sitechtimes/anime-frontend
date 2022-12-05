bc2eff8e9f98314fbd94dbafd89b9ab1e946ad48
MC4002 <maygec@nycstudents.net>
2022-12-05 12:27:27 -0500

    12/05 - HTML & CSS Edits

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="carousel">
-    <div class="carousel-slide">
+    <div class="carousel-slides">
     <input type="radio" name="btn" id="item-1" />
     <div class="carousel-display">
         <div class="carousel-item">
-            <h2 class="text">Title (1)</h2>
+            <h2 class="text">Title of Anime(1)</h2>
             <h3 class="text">Synopsis</h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD1"/>
         </div>
@@ -20,7 +20,7 @@
     <input type="radio" name="btn" id="item-2" checked/>
     <div class="carousel-display">
         <div class="carousel-item">
-            <h2 class="text">Title (2)</h2>
+            <h2 class="text">Title of Anime(2)</h2>
             <h3 class="text">Synopsis</h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD2"/>
         </div>
@@ -36,7 +36,7 @@
     <input type="radio" name="btn" id="item-3"/> 
     <div class="carousel-display">
         <div class="carousel-item">
-            <h2 class="text">Title (3)</h2>
+            <h2 class="text">Title of Anime(3)</h2>
             <h3 class="text">Synopsis</h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD3"/>
         </div>
@@ -65,39 +65,40 @@ export default {
 
 <style scoped>
 h2 {
-    margin-top: 2%;
+    margin-top: 2.5%;
     font-size: var(--h2);
 }
 h3 {
     font-size: var(--h3);
 }
 .img {
-    height: 40%;
+    height: 42%;
     float: right;
     margin-right: 6%;
     margin-bottom: 6%;
     vertical-align: middle;
 }
 .text {
-    clear: right;
     margin-left: 20%;
     text-align: left;
-    word-wrap: normal;
     color: var(--white);
     float: inline-start;
-    overflow-wrap: break-word;
 }
 input {
     display: none;
 }
 .controls label{ 
-    margin: 9% 3.5% 9% 3.5%;
     padding: 0;
+    opacity: 0.5;
     display: none;
     cursor: pointer;
-    font-size: 4rem;
+    font-size: 3.8rem;
     position: absolute;
     color: var(--white);
+    margin: 9% 3.5% 9% 3.5%;
+}
+.controls label:hover {
+    opacity: 1;
 }
 .controls .left {
     left: 0;
@@ -115,7 +116,7 @@ input {
 }
 .carousel-item {
     opacity: 0;
-    width: 83.5vw;
+    width: 85vw;
     height: 52%;
     display: block;
     margin-left: 5%;
@@ -124,11 +125,11 @@ input {
     transition: all .75s ease-in-out;
     background-color: var(--tertiary);
 }
-.carousel-slide {
-    white-space: nowrap;
-}
 .carousel-display {
     display: block;
+}
+.carousel-slides {
+    white-space: nowrap;
 }
 input:checked + .carousel-display .carousel-item {
     opacity: 1;
