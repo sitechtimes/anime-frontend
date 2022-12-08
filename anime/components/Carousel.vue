83d5298a8c5d7df2d102c443203bc101b6d57f84
MC4002 <maygec@nycstudents.net>
2022-12-08 12:28:53 -0500

    12/08 (2) - HTML & CSS Edits

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -1,51 +1,51 @@
 <template>
 <div class="carousel">
-    <div class="carousel-slides">
+    <div class="slides">
     <input type="radio" name="btn" id="item-1" />
-    <div class="carousel-display">
+    <div class="display">
         <div class="carousel-item">
-            <h2 class="text">Title of Anime (1)</h2>
-            <h3 class="text">Synopsis</h3>
+            <h2 class="text"> Title of Anime (1) </h2>
+            <h3 class="text"> Synopsis </h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD1"/>
         </div>
         <div class="controls">
             <label for="item-3" class="left">
-                <span>&#8249;</span>
+                <span> &#8249; </span>
             </label>
             <label for="item-2" class="right">
-                <span>&#8250;</span>
+                <span> &#8250; </span>
             </label>
         </div>
     </div>
     <input type="radio" name="btn" id="item-2" checked/>
-    <div class="carousel-display">
+    <div class="display">
         <div class="carousel-item">
-            <h2 class="text">Title of Anime (2)</h2>
-            <h3 class="text">Synopsis</h3>
+            <h2 class="text"> Title of Anime (2) </h2>
+            <h3 class="text"> Synopsis </h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD2"/>
         </div>
         <div class="controls">
             <label for="item-1" class="left">
-                <span>&#8249;</span>
+                <span> &#8249; </span>
             </label>
             <label for="item-3" class="right">
-                <span>&#8250;</span>
+                <span> &#8250; </span>
             </label>
         </div>
     </div>
     <input type="radio" name="btn" id="item-3"/> 
-    <div class="carousel-display">
+    <div class="display">
         <div class="carousel-item">
-            <h2 class="text">Title of Anime (3)</h2>
-            <h3 class="text">Synopsis</h3>
+            <h2 class="text"> Title of Anime (3) </h2>
+            <h3 class="text"> Synopsis </h3>
             <img class="img" src="https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg" alt="TBD3"/>
         </div>
         <div class="controls">
             <label for="item-2" class="left">
-                <span>&#8249;</span>
+                <span> &#8249; </span>
             </label>
             <label for="item-1" class="right">
-                <span>&#8250;</span>
+                <span> &#8250; </span>
             </label>
         </div>
     </div>
@@ -65,29 +65,32 @@ export default {
 
 <style scoped>
 h2 {
-    margin-top: 2.5%;
     font-size: var(--h2);
+    display: inline-block;
 }
 h3 {
-    max-width: 40vw;
+    display: block;
+    max-width: 33vw;
+    white-space: normal;
     font-size: var(--h3);
-    word-break: break-all;
+    overflow-wrap: break-word;
 }
 .img {
-    height: 50%;
+    clear: both;
+    height: 55%;
+    margin: auto;
     float: right;
-    margin-left: 1%;
-    margin-right: 16%;
-    vertical-align: middle;
+    display: block;
 }
 .text {
-    margin-left: 20%;
-    margin-right: 1%;
-    text-align: left;
+    margin-left: 10%;
     color: var(--white);
 }
 input {
     display: none;
+}
+.display {
+    display: block;
 }
 .controls .left {
     left: 0;
@@ -120,6 +123,7 @@ input {
     opacity: 0;
     width: 85vw;
     height: 58%;
+    padding: 1%;
     display: block;
     margin-left: 5%;
     margin-right: 5%;
@@ -127,50 +131,19 @@ input {
     transition: all .75s ease-in-out;
     background-color: var(--tertiary);
 }
-.carousel-slides {
+.slides {
     white-space: nowrap;
 }
-.carousel-display {
+input:checked + .display .nav label {
     display: block;
 }
-input:checked + .carousel-display .nav label {
+input:checked + .display .controls label {
     display: block;
 }
-input:checked + .carousel-display .controls label {
-    display: block;
-}
-input:checked + .carousel-display .carousel-item {
+input:checked + .display .carousel-item {
     opacity: 1;
     transform: scale(1);
     transition: 1.55s ease-in-out;
 }
-
-/* .carousel-display > carousel-item{
-
-} */
-
-/* keyframes animating carosal */
-/* @keyframes display {
-  0% {
-    transform: translateX(200px);
-    opacity: 0;
-  }
-  10% {
-    transform: translateX(0);
-    opacity: 1;
-  }
-  20% {
-    transform: translateX(0);
-    opacity: 1;
-  }
-  30% {
-    transform: translateX(-200px);
-    opacity: 0;
-  }
-  100% {
-    transform: translateX(-200px);
-    opacity: 0;
-  } 
- } */
 </style>
 
