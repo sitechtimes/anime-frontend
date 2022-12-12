f6dbd6b0671995503cfdb1a67eb4f95ee72252a5
Anqi-Tech <anqihu1107@gmail.com>
2022-12-12 12:31:05 -0500

    created login page

---
+++ anime/routes/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="login">
+    <form action="" class="login-form">
+      <h1>Technime</h1>
+      <input type="text">
+      <input type="text">
+      <p>Forget your password?</p>
+      <button>Login</button>
+      <p>Need to register?</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components:{
+  
+  }
+}
+</script>
+
+<style scoped>
+.login-form {
+    color: var(--white);
+    background: var(--primary);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 70vh;
+    width: 30vw;
+    border-radius: 30px;
+}
+</style>
