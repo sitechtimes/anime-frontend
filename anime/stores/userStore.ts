04aed2bc078a9ad50e70d8ff655b56d714b10456
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-12-20 16:40:46 -0500

    asx

--- anime/stores/userStore.ts
+++ anime/stores/userStore.ts
@@ -87,7 +87,7 @@ export const useUserStore = defineStore("user", {
         this.email = null
         this.isAuthenticated = false;
         this.token = null
-        googleLogout();
+        // googleLogout();
       } catch(error){
         console.error(error)
       }
