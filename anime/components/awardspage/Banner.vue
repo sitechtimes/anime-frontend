cdec4c99a26f8a9679662cbac84d7ce74e838e84
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-12-22 11:45:41 -0500

    Banner

---
+++ anime/components/awardspage/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="banner">
+    <div class="banner-bg">
+        <div class="banner-text">
+            <h2 class="trending-text">#trending</h2>
+            <h1 class="title-banner">vote for your faves anytime</h1>
+            <p class="voting-description">Mission Statement and more writing yeah</p>
+            <!-- <img src="" alt="Anya"> -->
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.banner-bg{
+    background-color: var(--primary);
+}
+</style>
