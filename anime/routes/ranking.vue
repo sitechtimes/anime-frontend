1da26cef0343c524eea28548e2c1bcd80bfa6e6f
KingJelloX <90670468+KingJelloX@users.noreply.github.com>
2023-01-03 12:29:31 -0500

    started ranking page

--- anime/routes/ranking.vue
+++ anime/routes/ranking.vue
@@ -1,16 +1,15 @@
 <template>
-  <div id="rankings">
-  </div>
+	<div id="rankings">
+		<h2>Top Anime of the Month</h2>
+    <div class="monthly-ranking-box"></div>
+    <div class="monthly-ranking-graph"></div>
+	</div>
 </template>
 
 <script lang="ts">
-export default ({
-  name: 'ranking',
-  components:{
-    
-  }
-})
+export default {
+	name: "ranking",
+	components: {},
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
