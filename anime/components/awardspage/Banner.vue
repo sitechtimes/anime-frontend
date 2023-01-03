620adc1effb20518cf2e90e6b0695a0b5cb96d92
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2023-01-03 12:30:06 -0500

    did absolutely nbothing, my coding speed is just slowly dying off

--- anime/components/awardspage/Banner.vue
+++ anime/components/awardspage/Banner.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="banner">
-    <div class="banner-bg">
+    <span class="underNav"></span>
+    <div class="banner-flex">
+      <div class="banner-bg">
         <div class="banner-text">
             <h2 class="trending-text">#trending</h2>
             <h1 class="title-banner">vote for your faves anytime</h1>
@@ -8,6 +10,7 @@
             <!-- <img src="" alt="Anya"> -->
         </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -18,7 +21,39 @@ export default {
 </script>
 
 <style>
+#banner{
+  width:100vw;
+  height:100vh;
+}
 .banner-bg{
     background-color: var(--primary);
+    width:40%;
+    height:55%;
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    color:var(--white);
+}
+.banner-flex{
+  width:80%;
+  height:45%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.banner-text{
+  width:55%;
+  height:90%;
+  margin-left:10%;
+  display:flex;
+  justify-content: space-around;
+  flex-direction: column;
+  background-color: var(--secondary);
+  border-radius: 25px;
+}
+.underNav{
+  width:100vw;
+  height:10rem;
+  display:block;
 }
 </style>
