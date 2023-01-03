87bce772ffce72cf7a63b222becae1876ac874fc
Anqi-Tech <anqihu1107@gmail.com>
2023-01-03 12:29:46 -0500

    seasonal awards

--- anime/routes/awards.vue
+++ anime/routes/awards.vue
@@ -1,17 +1,45 @@
 <template>
     <div id="awards">
-      <h1>please</h1>
+      <div class="seasonal">
+        <div v-for="award in awards" :key="award.category" class="award-box">
+          <h1>{{award.category}}</h1>
+        </div>
+      </div>
     </div>
   </template>
   
   <script lang="ts">
-  export default({
+  export default ({
     name: 'awards',
     components:{
   
-    }
+    },
+    data: () => ({
+      awards: [
+        {category: "A"},
+        {category: "B"},
+        {category: "C"},
+        {category: "C"},
+        {category: "C"},
+        {category: "C"},
+      ]
+    })
   })
   </script>
   <style>
-
+  #awards {
+    padding-top: 9vh;
+  }
+  .seasonal {
+    display: flex;
+  }
+  .award-box {
+    background: red;
+    color: white;
+    height: 10vh;
+    width: 10vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   </style>
