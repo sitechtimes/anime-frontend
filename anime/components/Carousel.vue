5ba42649999364181af6f5f1573cdc964eb04d22
MC4002 <maygec@nycstudents.net>
2023-01-04 12:30:47 -0500

    01/04 - Carousel Edits

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -2,21 +2,21 @@
 <div class="carousel">
     <div class="slides">
         <div class="carousel-slide slide-01">
-            <h2 class="text"> Blue Lock </h2>
-            <h4 class="text"> Synopsis </h4>    
+            <h2 class="text"> Blue Lock </h2>  
+            <h5 class="text"> Synopsis </h5>    
             <img class="img" src="https://th.bing.com/th/id/R.e4651a3a9eed4e1e43044e638b076e1f?rik=KXg2eMKBaDhnOQ&pid=ImgRaw&r=0" alt="Img of Blue Lock Characters"/>
         </div>
         <div class="carousel-slide slide-02">
             <h2 class="text"> Jujutsu Kaisen </h2>
-            <h4 class="text"> Synopsis </h4>
+            <h5 class="text"> Synopsis </h5>
             <img class="img" src="https://th.bing.com/th/id/OIP.mblZRlAplxkavD9u4ZgS2AHaD5?pid=ImgDet&rs=1" alt="Img of Jujutsu Kaisen Characters"/>
         </div>
         <div class="carousel-slide slide-03">
             <h2 class="text"> Burning Kabaddi </h2>
-            <h4 class="text"> Synopsis </h4>
+            <h5 class="text"> Synopsis </h5>
             <img class="img" src="https://th.bing.com/th/id/OIP.pNP5th6ouY4RBF-LEKpskAHaJE?pid=ImgDet&rs=1" alt="Img of Burning Kabaddi Characters"/>
         </div>
-</div>
+    </div>
     <div class="dots">
         <label class="dot dot-01" ></label>
         <label class="dot dot-02" ></label>
@@ -40,40 +40,40 @@ h2 {
     display: block;
     font-size: var(--h2);
 }
-h4 {
-    max-width: 48%;
+h5 {
+    max-width: 44%;
     white-space: normal;
-    font-size: var(--h4);
+    font-size: var(--h5);
     display: inline-block;
     overflow-wrap: break-word;
 }
 .img {
-    width: 45%;
-    height: 75%;
+    width: 50%;
+    height: 77%;
     float: right;
     display: inline;
-    margin-right: 1.5%;
+    margin-right: 1%;
+    border-radius: 10%;
 } 
-/* Note: Dots have yet to be properly vertically aligned  */
 .dots {  
     left: 0;
-    top: 65%;
     right: 0;
+    top: 64.5%;
     z-index: 5;
     position: absolute;
     text-align: center;
 } 
 .dots .dot {
-    opacity: 0.35;
-    width: 2.6rem;
-    height: 2.6rem;
-    margin: 0.85rem;
+    opacity: 0.25;
+    width: 2.45rem;
+    height: 2.45rem;
     border-radius: 50%; 
     display: inline-block;
+    margin: 0.65% 0.75% 0% 0.75%;
     background-color: var(--white);
 }
 .text {
-    margin-left: 1.5%;
+    margin-left: 1%;
     color: var(--white);
 }
 .carousel {
@@ -87,7 +87,6 @@ h4 {
     z-index: 0;
     width: 92%;
     height: 66%;
-    padding: 0.15%;
     display: block;
     position: absolute;
     margin-left: 1.75%;
@@ -99,10 +98,10 @@ h4 {
         opacity: 1;
     }
     34%, 100% {
-        opacity: 0.35;
+        opacity: 0.25;
     }
 }
-@keyframes auto-carousel {
+@keyframes auto-slide {
     0%, 33% {
         z-index: 2;
         opacity: 1;
@@ -114,21 +113,31 @@ h4 {
 }
 /* animation: name duration timing-function delay iteration-count direction fill-mode play-state; */
 .dot-01 {
-    animation: auto-dot 18s ease-in-out 0s infinite normal none running;
+    animation: auto-dot 9s ease-in-out 0s infinite normal none running;
 }
 .dot-02 {
-    animation: auto-dot 18s ease-in-out 6s infinite normal none running;
+    animation: auto-dot 9s ease-in-out 3s infinite normal none running;
 }
 .dot-03 {
-    animation: auto-dot 18s ease-in-out 12s infinite normal none running;
+    animation: auto-dot 9s ease-in-out 6s infinite normal none running;
 }
 .slide-01 {
-    animation: auto-carousel 18s ease-in-out 0s infinite normal none running;
+    animation: auto-slide 9s ease-in-out 0s infinite normal none running;
 }
 .slide-02 {
-    animation: auto-carousel 18s ease-in-out 6s infinite normal none running;
+    animation: auto-slide 9s ease-in-out 3s infinite normal none running;
 }
 .slide-03 {
-    animation: auto-carousel 18s ease-in-out 12s infinite normal none running;
+    animation: auto-slide 9s ease-in-out 6s infinite normal none running;
 }
+/* .slide-01 {
+    animation: auto-slide 11s ease-in-out 0s infinite normal none running;
+    transition: 1s all ease-in-out;
+}
+.slide-02 {
+    animation: auto-slide 11s ease-in-out 4s infinite normal none running;
+}
+.slide-03 {
+    animation: auto-slide 11s ease-in-out 8s infinite normal none running;
+} */
 </style>
