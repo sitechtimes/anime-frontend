17014e528afe6bc07215e64b131a7e7819140b40
KingJelloX <90670468+KingJelloX@users.noreply.github.com>
2023-01-04 12:31:59 -0500

    made trendingCard

---
+++ anime/components/homepage/RankingCard.vue
@@ -0,0 +1,103 @@
+<template>
+	<div class="rankingCard">
+		<div class="rc-rank">{{ rank }}</div>
+		<div class="rc-info">
+			<div class="rc-title">{{ title }}</div>
+			<div class="rc-sub">
+				<div class="rc-studio">{{ studio }}</div>
+				<div class="rc-stars">
+					<StarSVG />
+					<p>{{ stars }}</p>
+				</div>
+			</div>
+		</div>
+		<div class="rc-img">
+			<img :src="`${img}`" alt="" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: "RankingCard",
+	components: {},
+	props: {
+		rank: {
+			type: Number,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		studio: {
+			type: String,
+			required: true,
+		},
+		stars: {
+			type: Number,
+			required: true,
+		},
+		img: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style>
+.rankingCard {
+    display: flex;
+    margin: auto;
+	align-items: center;
+	justify-content: space-between;
+	background-color: #fff;
+	box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+	margin-bottom: 3rem;
+    width: 70vw;
+    height: 12rem;
+}
+.rc-rank {
+	font-size: 1.5rem;
+	font-weight: 600;
+	color: #000;
+}
+.rc-info {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100%;
+}
+.rc-title {
+	font-size: 1.25rem;
+	font-weight: 600;
+	color: #000;
+}
+.rc-sub {
+	display: flex;
+	align-items: center;
+	column-gap: 1rem;
+}   
+.rc-studio {
+	font-size: 1rem;
+	font-weight: 400;
+	color: #000;
+}
+.rc-stars {
+	display: flex;
+	align-items: center;
+	column-gap: 0.25rem;
+}
+.rc-img {
+	width: 50%;
+    height: 100%;
+	border-radius: 0.5rem;
+	overflow: hidden;
+}
+.rc-img img {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+}
+</style>
--- anime/routes/ranking.vue
+++ anime/routes/ranking.vue
@@ -1,15 +1,129 @@
 <template>
 	<div id="rankings">
-		<h2>Top Anime of the Month</h2>
-    <div class="monthly-ranking-box"></div>
-    <div class="monthly-ranking-graph"></div>
+		<span class="underNav"></span>
+		<h2 id="rankings-title">Top Anime of the Month</h2>
+		<div class="monthly-ranking-box">
+			<RankingCard
+				v-for="anime in topAnimeRank"
+				:key="anime.id"
+				:rank="anime.rank"
+				:title="anime.title"
+				:studio="anime.studio"
+				:stars="anime.stars"
+				:img="anime.img"
+			/>
+		</div>
+		<div class="monthly-ranking-graph"></div>
 	</div>
 </template>
 
 <script lang="ts">
+import RankingCard from "@/components/homepage/RankingCard.vue";
+
 export default {
 	name: "ranking",
-	components: {},
+	components: {
+		RankingCard,
+	},
+	data() {
+		return {
+			topAnimeRank: [
+				{
+					id: 1,
+					rank: 1,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 2,
+					rank: 2,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 3,
+					rank: 3,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 4,
+					rank: 4,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 5,
+					rank: 5,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 6,
+					rank: 6,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 7,
+					rank: 7,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 8,
+					rank: 8,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 9,
+					rank: 9,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+				{
+					id: 10,
+					rank: 10,
+					title: "One Piece",
+					studio: "Toei Animation",
+					stars: 5,
+					img: "https://sportshub.cbsistatic.com/i/2021/03/18/fc9eba90-ae7a-4e45-bef9-037145add04d/one-piece-wano-arc-1232360.jpg",
+				},
+			],
+		};
+	},
 };
 </script>
-<style scoped></style>
+
+<style scoped>
+.underNav {
+	width: 100%;
+	height: 10rem;
+	display: block;
+}
+#rankings-title {
+	color: #fff;
+	font-size: 2rem;
+	font-weight: 700;
+	margin-bottom: 1rem;
+}
+</style>
