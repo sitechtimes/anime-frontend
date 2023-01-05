03c7cf9ff07f3d5146f318f25b3c7bfa52b86bf8
MC4002 <maygec@nycstudents.net>
2023-01-05 11:46:32 -0500

    01/05 - Fade Animation +Image Edits

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -2,19 +2,19 @@
 <div class="carousel">
     <div class="slides">
         <div class="carousel-slide slide-01">
+            <img class="img" src="https://th.bing.com/th/id/R.e4651a3a9eed4e1e43044e638b076e1f?rik=KXg2eMKBaDhnOQ&pid=ImgRaw&r=0" alt="Img of Blue Lock Characters"/>
             <h2 class="text"> Blue Lock </h2>  
             <h5 class="text"> Synopsis </h5>    
-            <img class="img" src="https://th.bing.com/th/id/R.e4651a3a9eed4e1e43044e638b076e1f?rik=KXg2eMKBaDhnOQ&pid=ImgRaw&r=0" alt="Img of Blue Lock Characters"/>
         </div>
         <div class="carousel-slide slide-02">
+            <img class="img" src="https://th.bing.com/th/id/OIP.mblZRlAplxkavD9u4ZgS2AHaD5?pid=ImgDet&rs=1" alt="Img of Jujutsu Kaisen Characters"/>
             <h2 class="text"> Jujutsu Kaisen </h2>
             <h5 class="text"> Synopsis </h5>
-            <img class="img" src="https://th.bing.com/th/id/OIP.mblZRlAplxkavD9u4ZgS2AHaD5?pid=ImgDet&rs=1" alt="Img of Jujutsu Kaisen Characters"/>
         </div>
         <div class="carousel-slide slide-03">
+            <img class="img" src="https://th.bing.com/th/id/OIP.frbPaUGvaphnnZ8AWM-T8gHaKf?pid=ImgDet&rs=1" alt="Img of Burning Kabaddi Characters"/>
             <h2 class="text"> Burning Kabaddi </h2>
             <h5 class="text"> Synopsis </h5>
-            <img class="img" src="https://th.bing.com/th/id/OIP.pNP5th6ouY4RBF-LEKpskAHaJE?pid=ImgDet&rs=1" alt="Img of Burning Kabaddi Characters"/>
         </div>
     </div>
     <div class="dots">
@@ -41,32 +41,32 @@ h2 {
     font-size: var(--h2);
 }
 h5 {
-    max-width: 44%;
+    max-width: 45%;
+    display: block;
     white-space: normal;
     font-size: var(--h5);
-    display: inline-block;
     overflow-wrap: break-word;
 }
 .img {
+    margin: 1%;
     width: 50%;
-    height: 77%;
+    height: 95%;
     float: right;
-    display: inline;
-    margin-right: 1%;
-    border-radius: 10%;
+    display: block;
+    border-radius: 6%;
 } 
 .dots {  
     left: 0;
     right: 0;
-    top: 64.5%;
+    top: 65.5%;
     z-index: 5;
     position: absolute;
     text-align: center;
 } 
 .dots .dot {
     opacity: 0.25;
-    width: 2.45rem;
-    height: 2.45rem;
+    width: 2.5rem;
+    height: 2.5rem;
     border-radius: 50%; 
     display: inline-block;
     margin: 0.65% 0.75% 0% 0.75%;
@@ -94,21 +94,50 @@ h5 {
     background-color: var(--tertiary);
 }
 @keyframes auto-dot {
-    0%, 33% {
+    0%, 34% {
         opacity: 1;
     }
-    34%, 100% {
+    35%, 100% {
         opacity: 0.25;
     }
 }
+/* Carousel Slide Animation -> Fade  */
 @keyframes auto-slide {
-    0%, 33% {
+    0%, 27% {
         z-index: 2;
         opacity: 1;
     }
-    34%, 100% {
+    28% {
+        z-index: 2;
+        opacity: 0.8;
+    }
+    30% {
+        z-index: 2;
+        opacity: 0.6;
+    }
+    32% {
+        z-index: 2;
+        opacity: 0.4;
+    }
+    34% {
+        z-index: 2;
+        opacity: 0.2;
+    }
+    35%, 95% {
         z-index: 0;
         opacity: 0;
+    }
+    96% {
+        z-index: 1;
+        opacity: 0.33;
+    }
+    98% {
+        z-index: 1;
+        opacity: 0.66;
+    }
+    100% {
+        z-index: 1;
+        opacity: 0.99;
     }
 }
 /* animation: name duration timing-function delay iteration-count direction fill-mode play-state; */
@@ -130,14 +159,4 @@ h5 {
 .slide-03 {
     animation: auto-slide 9s ease-in-out 6s infinite normal none running;
 }
-/* .slide-01 {
-    animation: auto-slide 11s ease-in-out 0s infinite normal none running;
-    transition: 1s all ease-in-out;
-}
-.slide-02 {
-    animation: auto-slide 11s ease-in-out 4s infinite normal none running;
-}
-.slide-03 {
-    animation: auto-slide 11s ease-in-out 8s infinite normal none running;
-} */
 </style>
