d9ca1dc3cffd5df8abbf15892dd3c3e9dc32c1c8
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2023-01-06 12:32:00 -0500

    .

--- anime/components/awardspage/Banner.vue
+++ anime/components/awardspage/Banner.vue
@@ -12,7 +12,21 @@
     </div>
     <div class="banner-season">
       <h2 class="season-text">Anime of the Season</h2>
-      
+      <div v-for="anime in animes" :key="anime.title" class="bestAnime">
+          <img class="anime-pfp" src="https://cdn.myanimelist.net/images/characters/4/457933.jpg" alt="">
+          <div class="info-column">
+            <h1 class="title">{{anime.title}}</h1>
+              <div class="star-rating">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" class="star" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.0946 1.60341C14.6868 -0.25269 17.3132 -0.252693 17.9054 1.60341L20.2772 9.03723C20.5418 9.86642 21.3122 10.4293 22.1826 10.4293H29.9343C31.8629 10.4293 32.6743 12.89 31.1239 14.0371L24.7903 18.7232C24.1021 19.2324 23.8143 20.1232 24.0745 20.9389L26.4789 28.4746C27.0689 30.3237 24.9442 31.8447 23.384 30.6903L17.1896 26.1072C16.4827 25.5842 15.5173 25.5842 14.8104 26.1072L8.61604 30.6903C7.0558 31.8447 4.93115 30.3237 5.5211 28.4746L7.92546 20.9389C8.1857 20.1232 7.89791 19.2324 7.20966 18.7232L0.87612 14.0371C-0.674271 12.89 0.137065 10.4293 2.06569 10.4293H9.81743C10.6878 10.4293 11.4582 9.86642 11.7228 9.03723L14.0946 1.60341Z" fill="#FFEB34"/>
+                </svg>
+                <p>{{anime.stars.toFixed(2)}}</p>
+              </div>
+              <p>{{anime.genre}}</p>
+            </div>
+        </div>
+
+
     </div>
   </div>
   </div>
@@ -20,7 +34,20 @@
 
 <script>
 export default {
-
+  data: ()=> ({
+    animes: [
+      {title: "One Punch Man", stars: 8.50, releaseDate: "Oct 5, 2015"},
+      {title: "Spy x Family", stars: 9.99, releaseDate: "Apr 9, 2022"},
+      {title: "Chainsaw Man", stars: 8.24, releaseDate: "Oct 11, 2022"},
+      {title: "Summertime Render", stars: 9.61, releaseDate: "Apr 15, 2022"},
+      {title: "Is It Wrong To Try To Pick Up Girls In A Dungeon?", stars: 7.69, releaseDate: "Apr 4, 2015"},
+      {title: "I Spent Hours of my Life Doing Spiral Abyss and all I got was an Extra 50 Primogems", stars: 9.89, releaseDate: "June 9, 2022"},
+      {title: "Some Mid Isekai Anime", stars: 2.56, releaseDate: "Aug 19, 2021"},
+      {title: "I went on a Field Trip Where I Touched Grass For the First Time in Years", stars: 7.86, releaseDate: "Jan 17, 2012"},
+      {title: "The Pi in the Sky", stars: 9.90, releaseDate: "Mar 14, 1592"},
+      {title: "Genshin Impact The Anime", stars: 9.27, releaseDate: "TBA"},
+      ],
+  })
 }
 </script>
 
@@ -101,6 +128,7 @@ export default {
   font-weight: var(--fw-reg);
   margin-top:4%;
 }
+
 .underNav{
   width:100vw;
   height:15rem;
