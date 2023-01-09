0862233b89393523cf2881b25ffad3c6101367a6
Anqi-Tech <anqihu1107@gmail.com>
2023-01-09 12:30:49 -0500

    nested array works

--- anime/routes/awards.vue
+++ anime/routes/awards.vue
@@ -6,10 +6,8 @@
             <h1>{{award.category}}</h1>
           </div>
         </div>
-        <div class="nominee-container">
-          <!-- <div v-for="n in nominees" :key="n.text" class="nominee-box">
-            <h1>{{n.animes}}</h1>
-          </div> -->
+        <div v-for="award in awards" :key="award.category" class="nominee-container">
+          <div v-for="s in award.nominees" :key="s.title" class="nominee-box">{{s.title}}</div>
         </div>
       </div>
     </div>
@@ -23,24 +21,14 @@
     },
     data: () => ({
       awards: [
-        {category: "A"},
-        {category: "B"},
-        {category: "C"},
-        {category: "D"},
-        {category: "E"},
-        {category: "F"},
-        {category: "G"},
-        {category: "H"},
-      ],
-      nominees: [
-        {animes: ["A1", "A2", "A3"]},
-        {animes: [{title: "B1"}, {title: "B2"}, {title: "B3"}]},
-        {animes: [{title: "C1"}, {title: "C2"}, {title: "C3"}]},
-        {animes: [{title: "D1"}, {title: "D2"}, {title: "D3"}]},
-        {animes: [{title: "E1"}, {title: "E2"}, {title: "E3"}]},
-        {animes: [{title: "F1"}, {title: "F2"}, {title: "F3"}]},
-        {animes: [{title: "G1"}, {title: "G2"}, {title: "G3"}]},
-        {animes: [{title: "H1"}, {title: "H2"}, {title: "H3"}]},
+        {category: "A", nominees: [{title: "A1"}, {title: "A2"}, {title: "A3"}]},
+        {category: "B", nominees: [{title: "B1"}, {title: "B2"}, {title: "B3"}]},
+        // {category: "C"},
+        // {category: "D"},
+        // {category: "E"},
+        // {category: "F"},
+        // {category: "G"},
+        // {category: "H"},
       ]
     }),
     methods: {
