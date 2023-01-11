0176a57e3d13b830298a25b0755e0fa6d72fdc3f
MC4002 <maygec@nycstudents.net>
2023-01-11 12:30:29 -0500

    01/11 - Carousel HTML & CSS Edits

--- anime/components/Carousel.vue
+++ anime/components/Carousel.vue
@@ -1,9 +1,12 @@
 <template>
 <div class="carousel">
         <div class="carousel-slide slide-01">
-            <img class="img" src="https://th.bing.com/th/id/R.e4651a3a9eed4e1e43044e638b076e1f?rik=KXg2eMKBaDhnOQ&pid=ImgRaw&r=0" alt="Img of Blue Lock Characters"/>
+            <img class="img" src="https://th.bing.com/th/id/R.e4651a3a9eed4e1e43044e638b076e1f?rik=KXg2eMKBaDhnOQ&pid=ImgRaw&r=0" alt="Image of Blue Lock Characters"/>
             <h2 class="text"> Blue Lock </h2>  
-            <h5 class="text"> Synopsis </h5>    
+            <h5 class="text"> In 2018, the Japanese national team finished 16th in the FIFA World Cup. As a result, the Japanese Football Union hires the football enigma Ego Jinpachi.
+                 His master plan to lead Japan to stardom is Blue Lock, a training regimen designed to create the world's greatest egotist striker. Those who fail Blue Lock will 
+                 never again be permitted to represent any Japanese team. Yoichi Isagi, an unknown high school football player who is conflicted about his playing style, decides to
+                 join the program in order to become the best egotistical striker in the world. </h5>    
             <div class="dots">
                 <label class="dot dot-01" ></label>
                 <label class="dot dot-02" ></label>
@@ -11,9 +14,13 @@
             </div>
         </div>
         <div class="carousel-slide slide-02">
-            <img class="img" src="https://th.bing.com/th/id/OIP.mblZRlAplxkavD9u4ZgS2AHaD5?pid=ImgDet&rs=1" alt="Img of Jujutsu Kaisen Characters"/>
-            <h2 class="text"> Jujutsu Kaisen </h2>
-            <h5 class="text"> Synopsis </h5>
+            <img class="img" src="https://wallpapercave.com/wp/wp4445507.jpg" alt="Image of Kimetsu no Yaiba Characters"/>
+            <h2 class="text"> Kimetsu no Yaiba </h2>
+            <h5 class="text"> Rumors have echoed from time immemorial about the Demons that lurk in the wilderness. According to legend, a Demon Slayer sometimes roams the night,
+                 battling blood thirsty Demons. Since his father's death, a boy named Tanjiro has taken it upon himself to support his family. Although the disaster had affected 
+                 their lives, they were happy. The family is murdered by a Demons during the Taisho Period in Japan, and the eldest son Tanjiro finds that his sister, Nezuko has 
+                 also been transformed into a Demon. The sister still shows signs of human emotion, to his surprise, an unusual trait when compared to other Demons. Tanjiro quickly 
+                 sets out on a journey to fight Demons and find a way to turn his sister back into a human. </h5>
             <div class="dots">
                 <label class="dot dot-01" ></label>
                 <label class="dot dot-02" ></label>
@@ -21,9 +28,11 @@
             </div>
         </div>
         <div class="carousel-slide slide-03">
-            <img class="img" src="https://th.bing.com/th/id/OIP.frbPaUGvaphnnZ8AWM-T8gHaKf?pid=ImgDet&rs=1" alt="Img of Burning Kabaddi Characters"/>
+            <img class="img" src="https://th.bing.com/th/id/OIP.frbPaUGvaphnnZ8AWM-T8gHaKf?pid=ImgDet&rs=1" alt="Image of Burning Kabaddi Characters"/>
             <h2 class="text"> Burning Kabaddi </h2>
-            <h5 class="text"> Synopsis </h5>
+            <h5 class="text"> Tatsuya Yoigoshi used to be a famous soccer player called “Unbeatable Yoigoshi,” but he decides to give up sports when he enters high school. But
+                then he’s approached by an athletic club looking to recruit him and gets into a friendly competition that will change the course of his high school life and
+                propel him back into sports as a kabaddi player! </h5>
             <div class="dots">
                 <label class="dot dot-01" ></label>
                 <label class="dot dot-02" ></label>
@@ -46,7 +55,7 @@ export default {
 <style scoped>
 h2 {
     display: block;
-    margin-top: 0.5%;
+    margin-top: 0.35%;
     font-size: var(--h2);
 }
 h5 {
@@ -57,25 +66,25 @@ h5 {
     overflow-wrap: break-word;
 }
 .img {
-    width: 52%;
-    height: 88%;
+    height: 89%;
+    width: 52.5%;
     float: right;
     display: block;
-    border-radius: 5.5%;
-    margin: 0.75% 1.3% 0% 1.3%;
+    border-radius: 7.25%;
+    margin: 0.55% 1.25% 0% 1.25%;
 } 
-.dots {  
+.dots {   
     left: 0;
     right: 0;
+    top: 91%;
     z-index: 5;
-    top: 91.25%;
     position: absolute;
     text-align: center;
 } 
 .dots .dot {
     opacity: 0.15;
-    width: 1.75rem;
-    height: 1.75rem;
+    width: 1.85rem;
+    height: 1.85rem;
     border-radius: 50%; 
     display: inline-block;
     margin: 0.75% 0.45% 0% 0.45%;
@@ -94,12 +103,12 @@ h5 {
 .carousel-slide {
     opacity: 0;
     z-index: 0;
-    width: 97%;
-    height: 57%;
+    width: 98%;
+    height: 52%;
     display: block;   
     margin-left: 1%;
     margin-right: 1%;
-    position: absolute;  
+    position: absolute; 
     background-color: var(--tertiary);
 }
 @keyframes auto-dot {
@@ -141,11 +150,11 @@ h5 {
     }
     98% {
         z-index: 3;
-        opacity: 0.66;
+        opacity: 0.67;
     }
     100% {
         z-index: 3;
-        opacity: 0.99;
+        opacity: 1;
     }
 }
 .dot-01 {
