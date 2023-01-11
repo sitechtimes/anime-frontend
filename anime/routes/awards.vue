1de5ae5aec55590f1f82faa915130f3c72bbf7be
Anqi-Tech <anqihu1107@gmail.com>
2023-01-11 12:34:15 -0500

    changing nominees displayed

--- anime/routes/awards.vue
+++ anime/routes/awards.vue
@@ -1,13 +1,17 @@
 <template>
     <div id="awards">
       <div class="seasonal">
-        <div class="award-container">
-          <div v-for="award in awards" :key="award.category" @click="showNominees" class="award-box">
+        <div v-if="displayAwards" class="award-container">
+          <div v-for="(award, index) in awards" :key="award.category" @click="showNominees" class="award-box">
             <h1>{{award.category}}</h1>
+            <p class="hide">{{index}}</p>
           </div>
         </div>
-        <div v-for="award in awards[index].nominees" :key="award.title" class="nominee-container">
-          <div class="nominee-box">{{award.title}}</div>
+        <div v-else>
+          <div v-for="award in awards[index].nominees" :key="award.title" class="nominee-container">
+            <div class="nominee-box">{{award.title}}</div>
+          </div>
+          <button @click="back">Back</button>
         </div>
       </div>
     </div>
@@ -23,23 +27,28 @@
       awards: [
         {category: "A", nominees: [{title: "A1"}, {title: "A2"}, {title: "A3"}]},
         {category: "B", nominees: [{title: "B1"}, {title: "B2"}, {title: "B3"}]},
-        // {category: "C"},
-        // {category: "D"},
-        // {category: "E"},
-        // {category: "F"},
-        // {category: "G"},
-        // {category: "H"},
+        {category: "C", nominees: [{title: "C1"}, {title: "C2"}, {title: "C3"}]},
+        {category: "D", nominees: [{title: "D1"}, {title: "D2"}, {title: "D3"}]},
+        {category: "E", nominees: [{title: "E1"}, {title: "E2"}, {title: "E3"}]},
+        {category: "F", nominees: [{title: "F1"}, {title: "F2"}, {title: "F3"}]},
+        {category: "G", nominees: [{title: "G1"}, {title: "G2"}, {title: "G3"}]},
+        {category: "H", nominees: [{title: "H1"}, {title: "H2"}, {title: "H3"}]},
       ],
       index: 0,
+      displayAwards: true,
     }),
     methods: {
-      showNominees() {
-        console.log(this.awards)
+      showNominees(selected) {
+        this.index = selected.target.lastElementChild.innerHTML
+        this.displayAwards = false
+      },
+      back() {
+        this.displayAwards = true
       }
     },
   })
   </script>
-  <style>
+  <style scoped>
   #awards {
     padding-top: 9vh;
   }
@@ -76,5 +85,8 @@
     padding: 1rem;
     margin-bottom: 2vh;
     width: 50%;
+  }
+  .hide {
+    display: none;
   }
   </style>
