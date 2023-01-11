8a70baf6ab1c3b90bca5c757444379f18533a2ee
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-01-11 18:25:24 -0500

    dcs

--- anime/stores/userStore.ts
+++ anime/stores/userStore.ts
@@ -55,10 +55,10 @@ export const useUserStore = defineStore("user", {
                 let account = res.data.email.slice(index + 1)
                 
                 //add teacher later
-                if (account != "nycstudents.net" || "schools.nyc.gov") {
-                  return alert("Need to login with your school account (i.e. nycstudents.net)")
+                // if (account != "nycstudents.net" || "schools.nyc.gov") {
+                //   return alert("Need to login with your school account (i.e. nycstudents.net)")
                   
-                }
+                // }
 
                 // localStorage.setItem("user", res.data.first_name)
                 // this.userData = localStorage.getItem("user")
