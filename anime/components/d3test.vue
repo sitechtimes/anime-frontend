71e2332d6434c6d8dd284b43e3090fef02466ff4
KingJelloX <90670468+KingJelloX@users.noreply.github.com>
2023-01-12 12:19:08 -0500

    d3 maybe works

--- anime/components/d3test.vue
+++ anime/components/d3test.vue
@@ -34,8 +34,6 @@ for (const keyframe of keyframes) {
 	await transition.end();
 }
 
-duration = 250;
-
 d3.group(data, d => d.name);
 
 n = 12;
@@ -61,6 +59,196 @@ function rank(value) {
 }
 
 rank(name => datevalues[0][1].get(name));
+
+k = 10;
+
+const keyframes = [];
+let ka, a, kb, b;
+for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+	for (let i = 0; i < k; ++i) {
+		const t = i / k;
+		keyframes.push([
+			new Date(ka * (1 - t) + kb * t),
+			rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t),
+		]);
+	}
+}
+keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
+
+nameframes = d3.groups(
+	keyframes.flatMap(([, data]) => data),
+	d => d.name
+);
+
+prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])));
+next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
+
+function bars(svg) {
+	let bar = svg.append("g").attr("fill-opacity", 0.6).selectAll("rect");
+
+	return ([date, data], transition) =>
+		(bar = bar
+			.data(data.slice(0, n), d => d.name)
+			.join(
+				enter =>
+					enter
+						.append("rect")
+						.attr("fill", color)
+						.attr("height", y.bandwidth())
+						.attr("x", x(0))
+						.attr("y", d => y((prev.get(d) || d).rank))
+						.attr("width", d => x((prev.get(d) || d).value) - x(0)),
+				update => update,
+				exit =>
+					exit
+						.transition(transition)
+						.remove()
+						.attr("y", d => y((next.get(d) || d).rank))
+						.attr("width", d => x((next.get(d) || d).value) - x(0))
+			)
+			.call(bar =>
+				bar
+					.transition(transition)
+					.attr("y", d => y(d.rank))
+					.attr("width", d => x(d.value) - x(0))
+			));
+}
+
+function labels(svg) {
+	let label = svg
+		.append("g")
+		.style("font", "bold 12px var(--sans-serif)")
+		.style("font-variant-numeric", "tabular-nums")
+		.attr("text-anchor", "end")
+		.selectAll("text");
+
+	return ([date, data], transition) =>
+		(label = label
+			.data(data.slice(0, n), d => d.name)
+			.join(
+				enter =>
+					enter
+						.append("text")
+						.attr(
+							"transform",
+							d =>
+								`translate(${x((prev.get(d) || d).value)},${y(
+									(prev.get(d) || d).rank
+								)})`
+						)
+						.attr("y", y.bandwidth() / 2)
+						.attr("x", -6)
+						.attr("dy", "-0.25em")
+						.text(d => d.name)
+						.call(text =>
+							text
+								.append("tspan")
+								.attr("fill-opacity", 0.7)
+								.attr("font-weight", "normal")
+								.attr("x", -6)
+								.attr("dy", "1.15em")
+						),
+				update => update,
+				exit =>
+					exit
+						.transition(transition)
+						.remove()
+						.attr(
+							"transform",
+							d =>
+								`translate(${x((next.get(d) || d).value)},${y(
+									(next.get(d) || d).rank
+								)})`
+						)
+						.call(g =>
+							g
+								.select("tspan")
+								.tween("text", d => textTween(d.value, (next.get(d) || d).value))
+						)
+			)
+			.call(bar =>
+				bar
+					.transition(transition)
+					.attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
+					.call(g =>
+						g
+							.select("tspan")
+							.tween("text", d => textTween((prev.get(d) || d).value, d.value))
+					)
+			));
+}
+
+function textTween(a, b) {
+	const i = d3.interpolateNumber(a, b);
+	return function (t) {
+		this.textContent = formatNumber(i(t));
+	};
+}
+
+formatNumber = d3.format(",d");
+
+function axis(svg) {
+	const g = svg.append("g").attr("transform", `translate(0,${margin.top})`);
+
+	const axis = d3
+		.axisTop(x)
+		.ticks(width / 160)
+		.tickSizeOuter(0)
+		.tickSizeInner(-barSize * (n + y.padding()));
+
+	return (_, transition) => {
+		g.transition(transition).call(axis);
+		g.select(".tick:first-of-type text").remove();
+		g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+		g.select(".domain").remove();
+	};
+}
+
+function ticker(svg) {
+	const now = svg
+		.append("text")
+		.style("font", `bold ${barSize}px var(--sans-serif)`)
+		.style("font-variant-numeric", "tabular-nums")
+		.attr("text-anchor", "end")
+		.attr("x", width - 6)
+		.attr("y", margin.top + barSize * (n - 0.45))
+		.attr("dy", "0.32em")
+		.text(formatDate(keyframes[0][0]));
+
+	return ([date], transition) => {
+		transition.end().then(() => now.text(formatDate(date)));
+	};
+}
+
+formatDate = d3.utcFormat("%Y");
+
+function colors() {
+	const scale = d3.scaleOrdinal(d3.schemeTableau10);
+	if (data.some(d => d.category !== undefined)) {
+		const categoryByName = new Map(data.map(d => [d.name, d.category]));
+		scale.domain(Array.from(categoryByName.values()));
+		return d => scale(categoryByName.get(d.name));
+	}
+	return d => scale(d.name);
+}
+
+color = colors();
+
+x = d3.scaleLinear([0, 1], [margin.left, width - margin.right]);
+
+y = d3
+	.scaleBand()
+	.domain(d3.range(n + 1))
+	.rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
+	.padding(0.1);
+
+height = margin.top + barSize * n + margin.bottom;
+
+barSize = 48;
+
+margin = { top: 16, right: 6, bottom: 6, left: 0 };
+
+d3 = require("d3@6");
 
 export default {
 	name: "d3test",
