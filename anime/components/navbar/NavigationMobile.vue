2b99e792875c873fdc4f7b42f4d71f45e2c2bbc9
MC4002 <maygec@nycstudents.net>
2023-02-13 12:31:25 -0500

    02/13 - NavBar Mobile Edits

--- anime/components/navbar/NavigationMobile.vue
+++ anime/components/navbar/NavigationMobile.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="mobile">
-    <div class="menu" v-class="{close: isClose}">
+    <div class="menu" v-class="{ close: isClose }">
           <ul class="mq-routers">
                 <li class="link" v-on:click="toggleOn()">
                     <NuxtLink to="/awards" class="link-href">Awards</NuxtLink>
@@ -49,76 +49,84 @@ export default {
 </script>
 
 <style>
-.nav-mobile{
-    background-color:var(--tertiary);
-    width:100%;
-    height:9vh;
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
-    align-self: stretch;
+.nav-mobile {
+  width: 100%;
+  height: 9vh;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+  justify-content: space-between;
+  background-color: var(--tertiary);
 }
-.log-in{
-  margin-right:2rem;
+.log-in {
+  width: 25%;
+  margin-right: 2rem;
 }
- .burger1,
-  .burger2,
-  .burger3{
-    width: 35px;
+/* .link-href {
+  margin: 5%
+} */
+.burger1,
+.burger2,
+.burger3 {
+  width: 35px;
   height: 5px;
-  background-color: white;
   margin: 6px 0;
+  z-index: 2000;
   transition: .25s;
-  z-index:2000;
-
-  }
-  #logo{
-        font-size: var(--h3);
-        color:var(--white);
-        transition: all 1s ease-in-out;-webkit-transition: all 1s ease-in-out;
-    }
-  .change{
-    z-index:2000;
-  }
-  .change .burger1 {
-    position: relative;
+  background-color: white;
+}
+#logo {
+  color: var(--white);
+  font-size: var(--h3); 
+  transition: all 1s ease-in-out;        
+  -webkit-transition: all 1s ease-in-out;
+}
+.link-href {
+  margin-top: 5%;
+}
+.change {
+  z-index: 2000;
+}
+.change .burger1 {
+  position: relative;
   transform: translate(0, 16px) rotate(-45deg);
 }
-
-  .change .burger2 {opacity: 0;}
-
-  .change .burger3 {
-    position: absolute;
-    transform: translate(0, -11px) rotate(45deg);
-  }
-  div .close {
-    transform: translateX(110%);
-    z-index:1999;
-  }
-  .navigation-icon{
-    cursor:pointer;
-    margin-left:2rem;
-  }
-  .menu{
-    background-color: var(--bg-primary);
-    overflow: hidden;
-    width:100%;
-    height:100%;
-    left:-110%;
-    position:fixed;
-    color: var(--white);
-    transition:.5s;
-    z-index:-1;
-  }
+.change .burger2 { 
+  opacity: 0;
+}
+.change .burger3 {
+  position: absolute;
+  transform: translate(0, -11px) rotate(45deg);
+}
+div .close {
+  z-index: 1999;
+  transform: translateX(110%);
+}
+.navigation-icon {
+  cursor: pointer;
+  margin-left: 2rem;
+}
+.menu {
+  z-index: -1;
+  left: -110%;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  transition: .5s;
+  overflow: hidden;
+  color: var(--white);
+  background-color: var(--bg-primary);
+}
   .mq-routers{
-    display:flex;
-    height:50%;
-    width:90%;
-    flex-direction: column;
-    color: var(--white);
-    font-size:var(--h3);
-    justify-content: center;
-    align-items: center;
-
-  }
+  width: 90%;
+  height: 50%;
+  display: flex;
+  align-items: left;
+  margin-left: 22%;
+  /* align-items: center; */
+  color: var(--white);
+  font-size: var(--h3);
+  flex-direction: column;
+  justify-content: center;
+}
 </style>
