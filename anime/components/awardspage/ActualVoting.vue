f4e87dd755cb22982d2687688c328cbf8d0e1ff3
Anqi-Tech <anqihu1107@gmail.com>
2023-02-17 12:31:58 -0500

    voting flexbox

--- anime/components/awardspage/ActualVoting.vue
+++ anime/components/awardspage/ActualVoting.vue
@@ -2,26 +2,28 @@
   <div id="actual-voting">
     <h1 class="award-name">Award Name</h1>
     <div class="nominee-container">
-      <div class="nominee-box">
+      <div v-for="r1 in r" class="nominee-box">
         <img class="image-placeholder" src="https://cdn.myanimelist.net/images/characters/4/457933.jpg" alt="">
-        <h1>Anime Title</h1>
+        <h1 class="anime-title">Anime Title {{r1}}</h1>
       </div>
     </div>
     <NuxtLink to="/awards">Back</NuxtLink>
+    <button>Vote</button>
   </div>
 </template>
   
-<script>
+<script lang="ts">
 export default ({
-  
+  data: () => ({
+    r: [1,2,3,4,5,6]
+  })
 })
 </script>
   
-<style>
+<style scoped>
 #actual-voting {
   padding-top: 9vh;
-  background: blue;
-  width: 75vw;
+  width: 70vw;
   margin: auto;
 }
 .award-name {
@@ -30,5 +32,34 @@ export default ({
   font-size: var(--h1);
   text-align: center;
   margin-top: 3rem;
+}
+.nominee-container {
+  margin-top: 5vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.nominee-box {
+  padding: 1rem;
+  width: 33vw;
+  height: 15vh;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  outline: solid 2px gray;
+  background: var(--tertiary);
+  margin-bottom: 3rem;
+}
+.nominee-box:hover {
+  background: red;
+}
+.image-placeholder {
+  height: 100%;
+  aspect-ratio: 3/4;
+  object-fit: cover;
+}
+.anime-title {
+  font-size: var(--h3);
+  margin-left: 1rem;
 }
 </style>
