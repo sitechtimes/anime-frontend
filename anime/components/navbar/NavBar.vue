c32a024bf32fee052413b23513c64d2f8c38057c
MC4002 <maygec@nycstudents.net>
2023-03-02 11:38:04 -0500

    03/02 - Navigation Bar

--- anime/components/navbar/NavBar.vue
+++ anime/components/navbar/NavBar.vue
@@ -2,7 +2,7 @@
     <section id="nav" class="nav">
         <nav class="navigation" :class="{ 'is-hidden': !showHeader }">
             <h1 id="logo">
-                <NuxtLink to="/">Technime</NuxtLink></h1>       
+                <NuxtLink to="/" class="home-link">Technime</NuxtLink></h1>       
                 <SearchBar/>
             <ul class="routers">
                 <li class="link">
@@ -17,8 +17,7 @@
 
                
                 <NuxtLink v-if="userStore.isAuthenticated == false" to="/login" class="link-href"> <button class="log-in button">Log In</button></NuxtLink>
-                <NuxtLink v-if="userStore.isAuthenticated == true" to="/profile" class="link-href"><img src="https://th.bing.com/th/id/R.945f33b643f2ceffcdae90fb57c61854?rik=Ox1dcxb6rfOngw&riu=http%3a%2f%2fgetdrawings.com%2ffree-icon-bw%2ffree-avatars-icons-25.png&ehk=i8VrSYUn0ZsIdAnnI51DInJ0WdIXd3ukvat6kwZC%2f3c%3d&risl=&pid=ImgRaw&r=0" alt="" class="profile-button"></NuxtLink>
-                <button @click="userStore.logout" v-if="userStore.isAuthenticated == true" class=" log-in button"> Logout</button>
+                <button @click="userStore.logout" v-if="userStore.isAuthenticated == true" class="log-in button"> Logout</button>
             </ul>
         </nav>
 
@@ -107,54 +106,110 @@ import { useUserStore } from '../../stores/userStore'
     })
 </script>
 <style scoped>
-.navigation{
-    background-color:var(--tertiary);
-    width:100%;
-    height:9vh;
-    display:flex;
-    justify-content:space-evenly;
-    align-items:center;
+.log-in {
+    width: 100%;
+}
+.navigation {
+    z-index: 5;
+    width: 100%;
+    height: 9vh;
+    padding: 1rem;
+    display: flex;
+    margin: 0 auto;
+    position: fixed;
+    align-items: center;
     align-self: stretch;
-    margin:0 auto;
-    position:fixed;
-    z-index:100;
-    padding:1rem;
-    transform: translate3d(0, 0, 0);
+    justify-content: space-evenly;
     transition: 0.1s all ease-out;
+    transform: translate3d(0, 0, 0);
+    background-color: var(--tertiary);
 }
 .routers{
         width:40%;
         display:flex;
-        justify-content:space-between;
+        justify-content:space-around;
+        align-items: center;
     }
 .navbar.navbar--hidden {
-  box-shadow: none;
-  transform: translate3d(0, -100%, 0);
+    box-shadow: none;
+    transform: translate3d(0, -100%, 0);
 }
-.profile-button{
-    width:4rem;
+#logo {
+    color: var(--white);
+    font-size: var(--h2);
+    transition: all 1s ease-in-out;-webkit-transition: all 1s ease-in-out;
 }
-    #logo{
-        font-size: var(--h2);
-        color:var(--white);
-        transition: all 1s ease-in-out;-webkit-transition: all 1s ease-in-out;
-    }
-
-    .link{
-        font-size: var(--h4);
-    }
-
-    .link:hover{
-        color:var(--tertiary); 
-    }
-
-
-    .navigation {
-        transform: translateY(0);
-        transition: transform 300ms linear;
-    }
-    .navigation.is-hidden {
-        transform: translateY(-100%);
-    }
-
+.link {
+    font-size: var(--h4);
+}
+.link:hover {
+    color:var(--tertiary); 
+}
+.navigation {
+    transform: translateY(0);
+    transition: transform 300ms linear;
+}
+.navigation.is-hidden {
+    transform: translateY(-100%);
+}
+@media screen and (max-width: 1285px) {
+.log-in {
+    width: 10rem;
+    padding: 0.6rem;
+    font-size: var(--h4);
+}
+.home-link {
+    font-size: var(--h3);
+}
+.link-href {
+    margin: 2px 5px;
+    font-size: var(--h4);
+}
+}
+@media screen and (max-width: 1025px) {
+.log-in {
+    width: 7rem;
+    font-size: var(--h5);
+}
+.link-href {
+    font-size: var(--h5);
+}
+}
+@media screen and (max-width: 915px) {
+.log-in {
+    width: 7.8rem;
+}
+.home-link {
+    padding: 0rem;
+    font-size: var(--h3);
+}
+.link-href {
+    margin: 1px 5px;
+    font-size: var(--h4);
+}
+}
+@media screen and (max-width: 825px) {
+.log-in {
+    width: 8.2rem;
+    font-size: 1.65rem;
+    margin-right: 5rem;
+}
+.home-link {
+    font-size: var(--h3);
+}
+.link-href {
+    margin: 1px 5px;
+    font-size: 1.7rem;
+}
+}
+@media screen and (max-width: 770px) {
+.log-in {
+    width: 7rem;
+    font-size: 1.5rem;
+}
+.link-href {
+    margin: 2px 5px;
+    font-size: var(--h5);
+}
+}
 </style>
