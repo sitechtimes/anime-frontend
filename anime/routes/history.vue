3fb7374c0845ad41135155160b6937debe80ab94
MC4002 <maygec@nycstudents.net>
2023-03-14 12:32:32 -0400

    03/14 - CSS Edits + Arrowhead Edits

--- anime/routes/history.vue
+++ anime/routes/history.vue
@@ -2,23 +2,25 @@
   <div id="history">
     <div class="year-history">
     <h1 class="title-history"> History for #### </h1>
-    <h3 class="history-arrow up-arrow"> &#11165; </h3>
-    <h3 class="history-arrow down-arrow"> &#11167; </h3>
-    <div class="user-history">
+    <!-- <h3 class="history-arrow up-arrow"> &#11165; </h3>
+    <h3 class="history-arrow down-arrow"> &#11167; </h3> -->
+    <h3 v-if="Show" class="history-arrow"> &#11167; </h3>
+    <h3 v-else class="history-arrow"> &#11165; </h3>
+    <div v-if="Show" class="user-history">
     <div class="anime-history">
-    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="Anime"/>
+    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="TBD"/>
     <h3 class="anime-title"> Title of Anime </h3>
     </div>
     <div class="anime-history">
-    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="Anime"/>
+    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="TBD"/>
     <h3 class="anime-title"> Title of Anime </h3>
     </div>
     <div class="anime-history">
-    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="Anime"/>
+    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="TBD"/>
     <h3 class="anime-title"> Title of Anime </h3>
     </div>
     <div class="anime-history">
-    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="Anime"/>
+    <img class="anime-img" src="https://th.bing.com/th/id/OIP.VVki1-MBUqwb4NXk-55pMgAAAA?pid=ImgDet&rs=1" alt="TBD"/>
     <h3 class="anime-title"> Title of Anime </h3>
     </div>
     </div>
@@ -29,7 +31,9 @@
 <script lang="ts">
 export default ({
   name: 'history',
-  data: ()=> ({}),
+  data: ()=> ({
+    Show: true,
+  }),
   components:{}
 })
 </script>
@@ -42,21 +46,22 @@ export default ({
   width: 35%;
   height: 55%;
 }
-.up-arrow {
+/* .up-arrow {
   display: none;
-}
-.down-arrow {
-  /* display: none; */
   display: inline-block;
 }
+.down-arrow {
+  display: none;
+  display: inline-block;
+} */
 .anime-title {
-  font-size: var(--h3);
+  font-size: var(--h4);
 }
 .year-history {
   width: 96%;
-  height: 25%;
   margin-left: 2%;
   margin-right: 2%;
+  margin-bottom: 1%;
   align-items: center;
   color: var(--white);
   white-space: nowrap;
@@ -67,15 +72,14 @@ export default ({
   flex-direction: row;
 }
 .history-arrow {
-  margin-left: 15%;
+  margin-left: 12%;
   font-size: var(--h2);
   justify-self: center;
+  display: inline-block;
 }
 .anime-history {
+  margin: 0.5%;
   display: flex;
-  margin-top: 1%;
-  margin-left: 1%;
-  margin-right: 1%;
   align-items: center;
   flex-direction: column;
 }
