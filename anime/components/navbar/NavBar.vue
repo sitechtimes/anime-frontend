db63fae7becbb063fcafbed1877267e0fe5ed915
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2023-03-17 12:18:11 -0400

    Small Change

--- anime/components/navbar/NavBar.vue
+++ anime/components/navbar/NavBar.vue
@@ -2,23 +2,37 @@
     <section id="nav" class="nav">
         <nav class="navigation" :class="{ 'is-hidden': !showHeader }">
             <h1 id="logo">
-                <NuxtLink to="/" class="home-link">Technime</NuxtLink></h1>       
-                <SearchBar/>
+                <NuxtLink to="/" class="home-link">Technime</NuxtLink>
+            </h1>
+            <SearchBar />
             <ul class="routers">
                 <li class="link">
                     <NuxtLink to="/awards" class="link-href">Awards</NuxtLink>
                 </li>
                 <li class="link">
-                    <NuxtLink to="/ranking" class="link-href">Rankings</NuxtLink>
+                    <NuxtLink to="/ranking" class="link-href"
+                        >Rankings</NuxtLink
+                    >
                 </li>
                 <li class="link">
                     <NuxtLink to="/history" class="link-href">History</NuxtLink>
                 </li>
-                <NuxtLink v-if="userStore.isAuthenticated == false" to="/login" class="link-href"> <button class="log-in button">Log In</button></NuxtLink>
-                <button @click="userStore.logout" v-if="userStore.isAuthenticated == true" class="log-in button"> Logout</button>
+                <NuxtLink
+                    v-if="userStore.isAuthenticated == false"
+                    to="/login"
+                    class="link-href"
+                >
+                    <button class="log-in button">Log In</button></NuxtLink
+                >
+                <button
+                    @click="userStore.logout"
+                    v-if="userStore.isAuthenticated == true"
+                    class="log-in button"
+                >
+                    Logout
+                </button>
             </ul>
         </nav>
-
     </section>
 </template>
 <script lang="ts">
@@ -27,86 +41,89 @@ import axios from "axios";
 import { useUserStore } from "../../stores/userStore";
 
 export default {
-	name: "NavBar",
-	components: {
-		SearchBar,
-	},
-	setup() {
-		const userStore = useUserStore();
-		// const router = useRoute()
-		// userStore.$persist()
-		// console.log(userStore.userData)
+    name: "NavBar",
+    components: {
+        SearchBar,
+    },
+    setup() {
+        const userStore = useUserStore();
+        // const router = useRoute()
+        // userStore.$persist()
+        // console.log(userStore.userData)
 
-		return {
-			userStore,
-			// router
-		};
-	},
+        return {
+            userStore,
+            // router
+        };
+    },
 
-	//     data: () => ({
-	//     showHeader: true,
-	//     lastScrollPosition: 0,
-	//     scrollOffset: 40,
-	//     user: null
-	//   }),
-	data() {
-		return {
-			showHeader: true,
-			lastScrollPosition: 0,
-			scrollOffset: 40,
-		};
-	},
-	mounted() {
-		this.lastScrollPosition = window.pageYOffset;
-		window.addEventListener("scroll", this.onScroll);
+    //     data: () => ({
+    //     showHeader: true,
+    //     lastScrollPosition: 0,
+    //     scrollOffset: 40,
+    //     user: null
+    //   }),
+    data() {
+        return {
+            showHeader: true,
+            lastScrollPosition: 0,
+            scrollOffset: 40,
+        };
+    },
+    mounted() {
+        this.lastScrollPosition = window.pageYOffset;
+        window.addEventListener("scroll", this.onScroll);
 
-		// console.log(this.user)
-	},
+        // console.log(this.user)
+    },
 
-	beforeDestroy() {
-		window.removeEventListener("scroll", this.onScroll);
-	},
-	methods: {
-		test() {},
-		// Toggle if navigation is shown or hidden
-		onScroll() {
-			if (window.pageYOffset < 0) {
-				return;
-			}
-			if (Math.abs(window.pageYOffset - this.lastScrollPosition) < this.scrollOffset) {
-				return;
-			}
-			this.showHeader = window.pageYOffset < this.lastScrollPosition;
-			this.lastScrollPosition = window.pageYOffset;
-		},
-		// signOut() {
-		//     const auth2 = gapi.auth2.getAuthInstance()
-		//     auth2.signOut().then(() => {
-		//       console.log("user is signed out")
-		//     })
-		// },
-		// callback(res: any) {
-		//     console.log(res.access_token)
-		//     axios.post("http://localhost:8000/social-login/google/", {access_token: res.access_token}).then((res) => {
-		//         console.log(res)
-		//         axios.get('http://127.0.0.1:8000/auth/user/', {headers:{"Authorization": `Bearer ${res.data.access_token}`}}).then((res)=>
-		//         {
-		//             console.log(res.data.first_name)
-		//             // this.user = res.data.first_name
-		//             this.user = res.data
-		//         }
-		//         )
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.onScroll);
+    },
+    methods: {
+        test() {},
+        // Toggle if navigation is shown or hidden
+        onScroll() {
+            if (window.pageYOffset < 0) {
+                return;
+            }
+            if (
+                Math.abs(window.pageYOffset - this.lastScrollPosition) <
+                this.scrollOffset
+            ) {
+                return;
+            }
+            this.showHeader = window.pageYOffset < this.lastScrollPosition;
+            this.lastScrollPosition = window.pageYOffset;
+        },
+        // signOut() {
+        //     const auth2 = gapi.auth2.getAuthInstance()
+        //     auth2.signOut().then(() => {
+        //       console.log("user is signed out")
+        //     })
+        // },
+        // callback(res: any) {
+        //     console.log(res.access_token)
+        //     axios.post("http://localhost:8000/social-login/google/", {access_token: res.access_token}).then((res) => {
+        //         console.log(res)
+        //         axios.get('http://127.0.0.1:8000/auth/user/', {headers:{"Authorization": `Bearer ${res.data.access_token}`}}).then((res)=>
+        //         {
+        //             console.log(res.data.first_name)
+        //             // this.user = res.data.first_name
+        //             this.user = res.data
+        //         }
+        //         )
 
-		//     })
+        //     })
 
-		// }
-	},
+        // }
+    },
 };
 </script>
 <style scoped>
-.log-in {
+/* .log-in {
     width: 100%;
-}
+} */
 .navigation {
     z-index: 5;
     width: 100%;
@@ -127,12 +144,12 @@ export default {
     display: flex;
     justify-content: space-between;
 }
-.routers{
-        width:40%;
-        display:flex;
-        justify-content:space-between;
-        align-items: center;
-    }
+.routers {
+    width: 40%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 .navbar.navbar--hidden {
     box-shadow: none;
     transform: translate3d(0, -100%, 0);
@@ -140,13 +157,14 @@ export default {
 #logo {
     color: var(--white);
     font-size: var(--h2);
-    transition: all 1s ease-in-out;-webkit-transition: all 1s ease-in-out;
+    transition: all 1s ease-in-out;
+    -webkit-transition: all 1s ease-in-out;
 }
 .link {
     font-size: var(--h4);
 }
 .link:hover {
-    color:var(--tertiary); 
+    color: var(--tertiary);
 }
 .navigation {
     transform: translateY(0);
@@ -156,63 +174,63 @@ export default {
     transform: translateY(-100%);
 }
 @media screen and (max-width: 1285px) {
-.log-in {
-    width: 10rem;
-    padding: 0.6rem;
-    font-size: var(--h4);
-}
-.home-link {
-    font-size: var(--h3);
-}
-.link-href {
-    margin: 2px 5px;
-    font-size: var(--h4);
-}
+    .log-in {
+        width: 10rem;
+        padding: 0.6rem;
+        font-size: var(--h4);
+    }
+    .home-link {
+        font-size: var(--h3);
+    }
+    .link-href {
+        margin: 2px 5px;
+        font-size: var(--h4);
+    }
 }
 @media screen and (max-width: 1025px) {
-.log-in {
-    width: 7rem;
-    font-size: var(--h5);
-}
-.link-href {
-    font-size: var(--h5);
-}
+    .log-in {
+        width: 7rem;
+        font-size: var(--h5);
+    }
+    .link-href {
+        font-size: var(--h5);
+    }
 }
 @media screen and (max-width: 915px) {
-.log-in {
-    width: 7.8rem;
-}
-.home-link {
-    padding: 0rem;
-    font-size: var(--h3);
-}
-.link-href {
-    margin: 1px 5px;
-    font-size: var(--h4);
-}
+    .log-in {
+        width: 7.8rem;
+    }
+    .home-link {
+        padding: 0rem;
+        font-size: var(--h3);
+    }
+    .link-href {
+        margin: 1px 5px;
+        font-size: var(--h4);
+    }
 }
 @media screen and (max-width: 825px) {
-.log-in {
-    width: 8.2rem;
-    font-size: 1.65rem;
-    margin-right: 5rem;
-}
-.home-link {
-    font-size: var(--h3);
-}
-.link-href {
-    margin: 1px 5px;
-    font-size: 1.7rem;
-}
+    .log-in {
+        width: 8.2rem;
+        font-size: 1.65rem;
+        margin-right: 5rem;
+    }
+    .home-link {
+        font-size: var(--h3);
+    }
+    .link-href {
+        margin: 1px 5px;
+        font-size: 1.7rem;
+    }
 }
 @media screen and (max-width: 770px) {
-.log-in {
-    width: 7rem;
-    font-size: 1.5rem;
-}
-.link-href {
-    margin: 2px 5px;
-    font-size: var(--h5);
-}
+    .log-in {
+        width: 7rem;
+        font-size: 1.5rem;
+    }
+    .link-href {
+        margin: 2px 5px;
+        font-size: var(--h5);
+    }
 }
 </style>
