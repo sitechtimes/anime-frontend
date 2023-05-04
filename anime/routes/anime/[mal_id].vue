112c2abaf76d856fcad78fe63232999372d26e60
Johnson Wang <johnsonwang529@gmail.com>
2023-05-04 13:03:51 -0400

    got params to work but hydration

---
+++ anime/routes/anime/[mal_id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        knsdlc
+    </div>
+</template>
+
+<script lang="ts" setup>
+  const route = useRoute()
+  const id = route.params.mal_id
+  console.log(route.params.mal_id)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
