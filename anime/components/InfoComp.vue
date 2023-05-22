e276fdf633971268660294509731f74bf7b8eb5f
Johnson Wang <johnsonwang529@gmail.com>
2023-05-22 13:58:08 -0400

    changed to setup

--- anime/components/InfoComp.vue
+++ anime/components/InfoComp.vue
@@ -99,6 +99,330 @@
   </div>
 </template>
 
+<script lang="ts" setup>
+import starSVG from "../components/starSVG.vue";
+import LineChart from "../components/LineChart.vue";
+import { useUserStore } from "~~/stores/userStore";
+import { ref, onMounted, watch } from "vue"
+
+let addList = ref(false)
+let watchStatus = ref("NOT_WATCHING")
+let rating = ref(0)
+let change = ref(false)
+let userAnime = ref(null)
+let ratingOne = ref([])
+let allRatings = ref([])
+let chartRatings = ref([])
+let loaded = ref(false)
+
+const userStore = useUserStore()
+
+let chartData = ref(
+  {
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      datasets: [
+        {
+          label: "Rating Distribution",
+          //random data
+          // data: Array.from({ length: 10 }, () => Math.floor(Math.random() * 100)),
+          data: [],
+          fill: "start",
+          borderColor: "rgb(75, 192, 192)",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          pointStyle: false,
+        },
+      ],
+    }
+)
+
+
+const chartOptions = ref(
+  {
+      scales: {
+        y: {
+          type: "linear",
+          min: 0,
+          beginAtZero: true,
+          drawticks: false,
+          grid: {
+            display: false,
+          },
+          display: false,
+          grace: "5%",
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+          font: {
+            size: 30,
+          },
+        },
+      },
+      plugins: {
+        legend: false,
+        title: {
+          display: true,
+          text: "Rating Distribution",
+          padding: {
+            top: 10,
+            bottom: 30,
+          },
+          font: {
+            size: 20,
+          },
+        },
+      },
+      interaction: {
+        mode: "point",
+        intersect: false,
+      },
+      elements: {
+        line: {
+          cubicInterpolationMode: "monotone",
+        },
+      },
+    },
+  
+)
+
+const props = defineProps({
+  imageUrl: String, 
+  mediaType: String,
+  episodes: Number,
+    status: String,
+    aired: String,
+    studios: Array,
+    genres: Array,
+    animeName: String,
+    synopsis: String,
+    characters: Array,
+    mal_id: String,
+    avgRating: Number,
+})
+
+async function getUserProfile() {
+      const endpoint = "http://127.0.0.1:8000/graphql/";
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userStore.token}`,
+      };
+      const graphqlQuery = {
+        query: `{
+  userAnimeData(id:${userStore.userID}) {
+
+   userAnime{
+    edges{
+      node{
+        anime{
+          animeName,
+		  malId
+        },
+        rating,
+        watchingStatus
+      }
+    }
+  }
+}
+
+
+         
+}`,
+        variables: {},
+      };
+      const options = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(graphqlQuery),
+      };
+      const response = await fetch(endpoint, options);
+      const mutationData = await response.json();
+
+      console.log(mutationData.data.userAnimeData.userAnime.edges);
+      mutationData.data.userAnimeData.userAnime.edges.forEach((element) => {
+        // console.log(element.node.anime.animeName)
+        console.log(element.node)
+        if (element.node.anime.malId == props.mal_id) {
+          userAnime.value = element.node;
+        }
+      });
+      // const userAnime = mutationData.data.userAnimeData.userAnime.edges.filter(node => {
+      // 	node.node.anime.animeName === "Your Lie in April"
+      // })
+      console.log(userAnime.value.watchingStatus);
+
+      if (userAnime.value.watchingStatus != "NOT_WATCHING") {
+        watchStatus.value = userAnime.value.watchingStatus
+      }
+      if (userAnime.value.rating > 0) {
+        change.value = true;
+      } else {
+        change.value = false;
+      }
+      console.log(change.value);
+    }
+
+    async function getAllRatings() {
+      const endpoint = "http://127.0.0.1:8000/graphql/";
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userStore.token}`,
+      };
+      const graphqlQuery = {
+        query: `{
+  specificUserAnime(id:${props.mal_id}){
+    rating
+    # watchingStatus
+  }
+}`,
+        variables: {},
+      };
+      const options = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(graphqlQuery),
+      };
+      const response = await fetch(endpoint, options);
+      const ratingData = await response.json();
+      console.log(ratingData.data.specificUserAnime);
+
+      // const test = ratingData.data.specificUserAnime.filter(object =>
+      // 	object.rating == 6
+      // )
+
+      // console.log(test)
+      for (let i = 1; i <= 10; i++) {
+        console.log(i);
+        allRatings.value.push([]);
+        allRatings.value[i - 1] = ratingData.data.specificUserAnime.filter(
+          (object) => object.rating == i
+        ).length;
+      }
+
+      console.log(allRatings.value);
+
+	  chartData.value.datasets[0].data = allRatings.value
+
+	  loaded.value = true
+	
+	  console.log(chartData.value.datasets[0].data)
+    }
+
+    function add() {
+      addList.value = !addList.value;
+      console.log(addList.value);
+      const hi = document.getElementById("watch");
+      if (addList.value == true) {
+        hi!.innerHTML = "Added to Watchlist";
+        hi!.style.background = "rgb(255, 120, 140)";
+        hi!.style.filter = "opacity(80%)";
+      } else {
+        hi!.innerHTML = "Add to watchlist";
+        hi!.style.background = "lightpink";
+        hi!.style.filter = "none";
+      }
+    }
+
+    watch(watchStatus, (newValue) => {
+      changeWatchStatus()
+    })
+
+    watch(rating, (newValue) => {
+      changeRating()
+    })
+    async function changeWatchStatus() {
+      console.log(watchStatus.value, userStore.userID, props.mal_id);
+      const endpoint = "http://127.0.0.1:8000/graphql/";
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userStore.token}`,
+      };
+      const graphqlQuery = {
+        query: `mutation{
+						userAnimeMutation(animeData:{animeId:"${props.mal_id}"}, userData:{userId:"${userStore.userID}"}, userAnimeData:{watchStatus: "${watchStatus.value}"}){
+    user{
+      user{
+        username
+      }
+    },
+    userAnime{
+      anime{
+        animeName
+      },
+      rating,
+      watchingStatus
+    },
+    anime{
+      animeName,
+      avgRating
+    }
+  }
+}`,
+        variables: {},
+      };
+      const options = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(graphqlQuery),
+      };
+      const response = await fetch(endpoint, options);
+      const mutationData = await response.json();
+      console.log(mutationData);
+
+
+
+    }
+    
+
+
+    async function changeRating() {
+      console.log(rating.value);
+      const endpoint = "http://127.0.0.1:8000/graphql/";
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userStore.token}`,
+      };
+      const graphqlQuery = {
+        query: `mutation{
+						userAnimeMutation(animeData:{animeId:"${props.mal_id}"}, userData:{userId:"${userStore.userID}"}, userAnimeData:{rating:${rating.value}}){
+    user{
+      user{
+        username
+      }
+    },
+    userAnime{
+      anime{
+        animeName
+      },
+      rating,
+      watchingStatus
+    },
+    anime{
+      animeName,
+      avgRating
+    }
+  }
+}`,
+        variables: {},
+      };
+      const options = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(graphqlQuery),
+      };
+      const response = await fetch(endpoint, options);
+      const mutationData = await response.json();
+      console.log(mutationData);
+      window.location.reload();
+    }
+  
+
+    onMounted(() => {
+      getUserProfile();
+    getAllRatings();
+})
+</script>
+<!-- 
 <script lang="ts">
 import starSVG from "../components/starSVG.vue";
 import LineChart from "../components/LineChart.vue";
@@ -427,7 +751,7 @@ export default {
     },
   },
 };
-</script>
+</script> -->
 
 <style scoped>
 #anime-info {
