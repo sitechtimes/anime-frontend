e448cf99435ff934419d2906d10bbcc024cb7e05
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2023-05-31 11:52:58 -0400

    .

--- anime/components/awardspage/AwardVoting.vue
+++ anime/components/awardspage/AwardVoting.vue
@@ -9,98 +9,99 @@
         </NuxtLink>
     </div>
     <div class="award-container">
-
-      <!-- this should be another compoenent -->
-      <NuxtLink :to="`/vote/` +  award" v-for="award in awards" :key="award">
-        <div class="award-box">
-          <img src="https://cdn.animenewsnetwork.com/hotlink/thumbnails/max700x700/cms/news.2/147637/001_size8.jpg" alt="" class="img">
-          <h1 class="award-name">{{award}}</h1>
-          <div class="fake-vote-btn">Vote</div>
-        </div>
-      </NuxtLink>
+        <!-- this should be another compoenent -->
+        <NuxtLink :to="`/vote/` + award" v-for="award in awards" :key="award">
+            <div class="award-box">
+                <img
+                    src="https://cdn.animenewsnetwork.com/hotlink/thumbnails/max700x700/cms/news.2/147637/001_size8.jpg"
+                    alt=""
+                    class="img"
+                />
+                <h1 class="award-name">{{ award }}</h1>
+                <div class="fake-vote-btn">Vote</div>
+            </div>
+        </NuxtLink>
     </div>
-  </div>
 </template>
-  
+
 <script lang="ts" setup>
 import { useUserStore } from "~~/stores/userStore";
-import { ref, onMounted} from "vue"
+import { ref, onMounted } from "vue";
 
-let awards = ref([])
-const userStore = useUserStore()
+let awards = ref([]);
+const userStore = useUserStore();
 
 onMounted(() => {
-     userStore.getAllAwards()
-    awards.value = userStore.allAwards
-
-})
+    userStore.getAllAwards();
+    awards.value = userStore.allAwards;
+});
 </script>
-  
+
 <style scoped>
 #award-voting {
-  margin: auto;
-  width: 75vw;
-  margin-bottom: 30vh;
-  color: var(--white);
+    margin: auto;
+    width: 75vw;
+    margin-bottom: 30vh;
+    color: var(--white);
 }
 .text-box {
-  margin-bottom: 4rem;
+    margin-bottom: 4rem;
 }
 .text-head {
-  font-size: var(--h2);
+    font-size: var(--h2);
 }
 .text-sub {
-  font-size: var(--h6);
-  display: flex;
+    font-size: var(--h6);
+    display: flex;
 }
 .text-tab {
-  margin-right: 3rem;
-  transition: 200ms ease-in-out;
+    margin-right: 3rem;
+    transition: 200ms ease-in-out;
 }
 .text-tab:hover {
-  color: var(--primary);
-  text-decoration: underline;
+    color: var(--primary);
+    text-decoration: underline;
 }
 .award-container {
-  display: grid;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
-  column-gap: 6rem;
-  row-gap: 6rem;
+    display: grid;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    column-gap: 6rem;
+    row-gap: 6rem;
 }
 .award-box {
-  display: flex;
-  flex-direction: column;
-  border-radius: 10px;
-  transition: 100ms ease-in;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    transition: 100ms ease-in;
 }
 .award-box:hover {
-  outline: solid 5px var(--primary);
-  background: var(--primary);
-  transform: translate(0, -8px);
+    outline: solid 5px var(--primary);
+    background: var(--primary);
+    transform: translate(0, -8px);
 }
 .img {
-  height: 20rem;
-  border-radius: 10px;
-  object-fit: cover;
+    height: 20rem;
+    border-radius: 10px;
+    object-fit: cover;
 }
 .award-name {
-  text-align: center;
-  font-size: var(--h4);
-  white-space: nowrap;
-  overflow: hidden;
-  display: block;
-  text-overflow: ellipsis;
+    text-align: center;
+    font-size: var(--h4);
+    white-space: nowrap;
+    overflow: hidden;
+    display: block;
+    text-overflow: ellipsis;
 }
 .fake-vote-btn {
-  background: var(--primary);
-  border-radius: 20px;
-  font-size: var(--h5);
-  text-align: center;
-  width: 10rem;
-  z-index: 1;
-  align-self: flex-end;
-  position: absolute;
-  margin: 1rem 0.5rem 0 0;
+    background: var(--primary);
+    border-radius: 20px;
+    font-size: var(--h5);
+    text-align: center;
+    width: 10rem;
+    z-index: 1;
+    align-self: flex-end;
+    position: absolute;
+    margin: 1rem 0.5rem 0 0;
 }
 
 @media screen and (max-width: 1440px) {
